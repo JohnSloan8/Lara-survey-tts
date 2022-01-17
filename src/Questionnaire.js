1cd19e1a25ae60410e51b35e0523196f7f34f42c
JohnSloan8 <78593708+JohnSloan8@users.noreply.github.com>
2022-01-17 10:22:03 +0000

    working with modularisation

---
+++ src/Questionnaire.js
@@ -0,0 +1,60 @@
+import { React, useCallback, useEffect, useState } from "react";
+
+import "survey-react/modern.min.css";
+// import 'survey-react/survey.min.css';
+import { Survey, StylesManager, Model } from "survey-react";
+import axios from "axios";
+import { render } from "react-dom";
+
+StylesManager.applyTheme("modern");
+
+// const basicJson = {
+//   elements: [
+//     {
+//       name: "FirstName",
+//       title: "Enter your first names:",
+//       type: "text"
+//     },
+//     {
+//       name: "LastName",
+//       title: "Enter your last name:",
+//       type: "text"
+//     }
+//   ]
+// };
+
+const Questionnaire = () => {
+  const [surveyJSON, setSurveyJSON] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios(
+      "https://api.surveyjs.io/public/Survey/getSurvey?surveyId=af7ca1cb-84ff-4005-a034-bd34ddd08c23"
+    ).then((json) => {
+      setSurveyJSON(json.data);
+      console.log("json.data:", json.data);
+      survey = new Model(json.data);
+      setLoading(false);
+      //displaySurvey();
+    });
+  }, []);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  console.log("survey:", survey);
+
+  // survey.focusFirstQuestionAutomatic = false;
+
+  // const alertResults = useCallback((sender) => {
+  //   const results = JSON.stringify(sender.data);
+  //   alert(results);
+  // }, []);
+
+  //survey.onComplete.add(alertResults);
+
+  return <Survey model={survey} />;
+};
+
+export default Questionnaire;
